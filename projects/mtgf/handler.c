6761425aa760a612d9f8fd38a83712ce1e9bb183
psuntharanund <pawat.suntharanund@gmail.com>
2026-02-11 11:27:36 -0500

    Updated MTGF

--- projects/mtgf/handler.c
+++ projects/mtgf/handler.c
@@ -9,12 +9,13 @@ static pthread_cond_t queue_cv = PTHREAD_COND_INITIALIZER;
 
 static steque_t queue = {0};
 static pthread_t *workers = NULL;
+static int threadCount = 0;
+static int queueingDone = 0;
 
 typedef struct job{
-    int jobid;
-    int sockfd; 
     char *path;
     gfcontext_t **ctx;
+    gfstatus_t status;
     gfh_error_t result;
     pthread_mutex_t done_mtx;
     pthread_cond_t done_cv;
@@ -31,18 +32,76 @@ typedef struct job{
 //  Note: you don't need to use arg. The test code uses it in some cases, but
 //        not in others.
 //
-void *worker_job(){
+static void *worker_job(void *arg){
+    (void)arg;
+    for (;;){
+        pthread_mutex_lock(&queue_mtx);
+        while ((steque_isempty(&queue) == 0) && (queueingDone == 0)){
+            pthread_cond_wait(&queue_cv, &queue_mtx);
+        }
+        if ((steque_isempty(&queue) == 0) && (queueingDone != 0)){
+            pthread_mutex_unlock(&queue_mtx);
+            pthread_cond_signal(&queue_cv);
+            break;
+        }
+        job_t * job = (job_t*)steque_pop(&queue);
+        pthread_mutex_unlock(&queue_mtx);
+        content_get(job->path);
+        int workfd = open(job->path, O_RDONLY);
+        if (workfd < 0){
+            gfs_sendheader(job->ctx, GF_FILE_NOT_FOUND, 0);
+            goto done;
+        }
+        struct stat fileStatus;
+        fstat(workfd, &fileStatus);
+        size_t fileLen = fileStatus.st_size;
+        gfs_sendheader(job->ctx, job->status, fileLen);
 
+        char buffer[1024];
+        ssize_t workConnection;
+        while ((workConnection = read(workfd, buffer, sizeof(buffer))) > 0){
+            if (gfs_send(job->ctx, buffer, (size_t)workConnection) < 0){
+                break;
+            }
+        }
+        close(workfd);
+        
+    done:
+        pthread_mutex_lock(&job->done_mtx);
+        job->complete = 1;
+        pthread_cond_broadcast(&job->done_cv);
+        pthread_mutex_unlock(&job->done_mtx);
+    }
+    return NULL;
 }
 
 void init_threads(size_t numthreads){
     steque_init(&queue);
-
+    threadCount = numthreads;
+    workers = malloc(numthreads * sizeof(*workers));
+    if (workers == 0){
+        exit(1);
+    }
     for (int i = 0; i < numthreads; i++){
         pthread_create(&workers[i], NULL, worker_job, NULL);
     }
 
-};
+}
+
+void cleanup_threads(){
+    pthread_mutex_lock(&queue_mtx);
+    queueingDone = 1;
+    pthread_cond_broadcast(&queue_cv);
+    pthread_mutex_unlock(&queue_mtx);
+
+    if (workers != 0){
+        for (int i = 0; i < threadCount; i++){
+            pthread_join(workers[i], NULL);
+        }
+            free(workers);
+            workers = NULL;
+    }
+}
 
 gfh_error_t gfs_handler(gfcontext_t **ctx, const char *path, void* arg){
 	
@@ -61,12 +120,14 @@ gfh_error_t gfs_handler(gfcontext_t **ctx, const char *path, void* arg){
     job->result = gfh_failure;
     
     pthread_mutex_lock(&queue_mtx);
-    steque_push(&queue, (steque_item)job);
+    steque_enqueue(&queue, (steque_item)job);
 
     pthread_mutex_unlock(&queue_mtx);
     pthread_cond_broadcast(&queue_cv);
 
     pthread_mutex_lock(&job->done_mtx);
+    job->complete = 1;
+    pthread_cond_signal(&job->done_cv);
     while (job->complete == 0){
         pthread_cond_wait(&job->done_cv, &job->done_mtx);
     }
